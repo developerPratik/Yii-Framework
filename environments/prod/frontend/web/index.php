682b8b43e94b9eb1f9ea6e05d32d86c7d8bacce4
developerPratik <pratik_star99@hotmail.com>
2017-03-19 15:40:34 +0545

    Update index.php

--- environments/prod/frontend/web/index.php
+++ environments/prod/frontend/web/index.php
@@ -12,6 +12,7 @@ $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main-local.php'),
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . '/../config/main-local.php')
+    echo 'timimeromatimromayachhokhochhahamro';
 );
 
 (new yii\web\Application($config))->run();
